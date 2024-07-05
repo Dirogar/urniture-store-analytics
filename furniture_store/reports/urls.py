fe3cfc92cb9952eb32705f52f39842e3017e3898
Max Ponomarenko <JustForLobster@gmail.com>
2024-07-05 17:59:09 +0500

    Добавлены комменты и возможность их обновлять (добавление с ошибкой)

--- furniture_store/reports/urls.py
+++ furniture_store/reports/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import MainPage
+from .views import MainPage, EditCommentView
 
 app_name = 'reports'
 
 urlpatterns = [
-    path('', MainPage.as_view(), name='index')
+    path('', MainPage.as_view(), name='index'),
+    path('comment/<int:pk>/edit/', EditCommentView.as_view(),
+         name='comment_edit'),
 ]
--- furniture_store/reports/views.py
+++ furniture_store/reports/views.py
@@ -1,12 +1,52 @@
 from django.views.generic import (
-    ListView, CreateView, UpdateView, DeleteView, DetailView
+    ListView, CreateView, UpdateView, DeleteView, DetailView, View
 )
-from django.contrib.auth.mixins import LoginRequiredMixin
+from http import HTTPStatus
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import redirect, get_object_or_404
+from django.urls import reverse
+from django.http import JsonResponse
 
-from .models import Shops
+from .models import Shops, Comment
+from .forms import CommentForm
 
 
 class MainPage(LoginRequiredMixin, ListView):
     model = Shops
     template_name = 'reports/shop_list.html'
     redirect_field_name = 'redirect_to'
+    context_object_name = 'shops'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment_form'] = CommentForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.shop_id = request.POST.get('shop_id')
+            comment.save()
+            return redirect(reverse('reports:index'))
+        return self.get(request, *args, **kwargs)
+
+
+class EditCommentView(LoginRequiredMixin, UserPassesTestMixin, View):
+    def post(self, request, *args, **kwargs):
+        comment = get_object_or_404(Comment, pk=kwargs['pk'])
+        if comment.author != request.user:
+            return JsonResponse({'error': 'Permission denied'},
+                                status=HTTPStatus.FORBIDDEN)
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({'success': 'Comment updated',
+                                 'comment_text': comment.text})
+        return JsonResponse({'error': 'Form is invalid'},
+                            status=HTTPStatus.BAD_REQUEST)
+
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
