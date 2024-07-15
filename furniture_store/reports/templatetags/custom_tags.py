4d14e530eb2f5a8fff2fba45054603faacb3223a
Max Ponomarenko <JustForLobster@gmail.com>
2024-07-15 16:05:14 +0500

    Добавлено отображение данных выставки магазина

--- furniture_store/reports/templatetags/custom_tags.py
+++ furniture_store/reports/templatetags/custom_tags.py
@@ -7,3 +7,13 @@ register = template.Library()
 def get_item(queryset, key):
     return queryset.get(key)
 
+
+@register.filter
+def default_if_none(queryset, key):
+    return queryset or 0
+
+
+@register.filter()
+def square(queryset: float):
+    return float(queryset*2)
+
