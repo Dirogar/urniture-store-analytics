671ca0b58e3721376ded2b6b97484a62bd176aa7
Max Ponomarenko <JustForLobster@gmail.com>
2024-07-22 13:18:59 +0500

    Добавлен валидатор даты комментария

--- furniture_store/reports/forms.py
+++ furniture_store/reports/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.forms import DateInput
+from django.utils import timezone
+from django.core.validators import ValidationError
 from .models import Comment
 
 
@@ -13,7 +15,21 @@ class CommentForm(forms.ModelForm):
                 format=('%Y-%m-%d'),
                 attrs={
                        'type': 'date',
-                       'placeholder': 'Select a date'}
+                       'placeholder': 'Select a date',
+                       'class': 'form-control'
+                }
             ),
         }
+
+    def clean_finish_planned_date(self):
+        """Проверяет что в форму передана валидная дата"""
+        finish_planned_date = self.cleaned_data.get('finish_planned_date')
+        current_date = timezone.now()
+        if finish_planned_date <= timezone.now():
+            raise ValidationError(
+                'Планируемая дата выполнения должна быть в будущем'
+            )
+        return finish_planned_date
+
+
 
