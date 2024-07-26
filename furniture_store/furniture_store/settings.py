f0a916674419ab8c78ef73297b17ec35ff5aa168
Max Ponomarenko <JustForLobster@gmail.com>
2024-07-26 15:38:34 +0500

    Добавлен CRFS_TRUSTED_ORIGINS

--- furniture_store/furniture_store/settings.py
+++ furniture_store/furniture_store/settings.py
@@ -22,10 +22,10 @@ DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
 DB_SCHEMA = os.getenv('DB_SCHEMA')
 SECRET_KEY = os.getenv('SECRET_KEY')
+CSRF_TRUSTED_DOMAIN = os.getenv('CSRF_TRUSTED_DOMAIN')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -37,7 +37,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-
 
 # Application definition
 
@@ -84,7 +83,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'furniture_store.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -97,11 +95,10 @@ DATABASES = {
         "HOST": DB_HOST,
         "PORT": DB_PORT,
         "OPTIONS": {
-        "options": f'-c search_path={DB_SCHEMA}'
-    },
+            "options": f'-c search_path={DB_SCHEMA}'
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -121,7 +118,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -133,18 +129,15 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static_dev',
 ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -153,3 +146,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'reports:products'
 
+
+CSRF_TRUSTED_ORIGINS = [
+    CSRF_TRUSTED_DOMAIN,
+]
