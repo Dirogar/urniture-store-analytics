f7219ca05c40d99f48ddc71660799fdc0e893bc0
Max Ponomarenko <JustForLobster@gmail.com>
2024-07-30 18:01:31 +0500

    Рефакторинг - убрана лишняя вложенность проектов

--- furniture_store/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-python furnitore_store/manage.py migrate
-
-python furnitore_store/manage.py collectstatic --noinput
-
-exec gunicorn furniture_store.wsgi:application --bind 127.0.0.1:8000 --workers 3
