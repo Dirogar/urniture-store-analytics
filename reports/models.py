8a5bd8b7078704643e2df3c905e0451d46e871b1
Max Ponomarenko <JustForLobster@gmail.com>
2024-08-02 14:08:59 +0500

    Добавлены докстринги. Код приведён к стандарту flake8

--- reports/models.py
+++ reports/models.py
@@ -1,31 +1,54 @@
 """Модели приложения reports."""
-import uuid
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from .validators import validate_future_date
-
 
 DEFAULT_AREA = 0
 
 
+class WorkPosition(models.Model):
+    """Должность сотрудника."""
+
+    name = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True,
+        verbose_name='Нзавние должности'
+    )
+
+    class Meta:
+        """Русское название модели."""
+
+        verbose_name = 'Должность'
+        verbose_name_plural = 'Должности'
+
+    def __str__(self):
+        """Возвращает название поля."""
+        return self.name
+
+
 class City(models.Model):
     """Модель города."""
+
     name = models.CharField(max_length=100,
                             null=False,
                             blank=False,
                             verbose_name='Название города')
+
     class Meta:
+        """Русское название модели."""
+
         verbose_name = 'Город'
         verbose_name_plural = 'Города'
 
     def __str__(self):
-        """Возвращает название города"""
+        """Возвращает название города."""
         return self.name
 
+
 class Warehouse(models.Model):
+    """Модель склада."""
+
     name = models.CharField(
         max_length=254,
         null=False,
@@ -33,15 +56,19 @@ class Warehouse(models.Model):
     )
 
     class Meta:
+        """Русское название модели."""
+
         verbose_name = 'Склад'
         verbose_name_plural = 'Склады'
 
     def __str__(self):
+        """Возвращает название поля."""
         return self.name
 
 
-
 class Store(models.Model):
+    """Модель магазина салона."""
+
     name = models.CharField(
         max_length=254,
         null=False,
@@ -60,16 +87,23 @@ class Store(models.Model):
     )
 
     class Meta:
+        """Русское название модели."""
+
         verbose_name = 'Магазин'
         verbose_name_plural = 'Магазины'
 
     def __str__(self):
+        """Возвращает название поля."""
         return self.name
+
+
 class User(AbstractUser):
-    work_position = models.CharField(
-        max_length=100,
+    """Дополненная модель пользователя."""
+
+    work_position = models.ForeignKey(
+        WorkPosition,
+        on_delete=models.DO_NOTHING,
         null=True,
-        blank=True,
         verbose_name='Должность'
     )
     store = models.ManyToManyField(
@@ -80,12 +114,14 @@ class User(AbstractUser):
         Group,
         related_name='custom_user_set',  # Уникальное имя для related_name
         blank=True,
-        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        help_text='The groups this user belongs to. A user will get all '
+                  'permissions granted to each of their groups.',
         verbose_name='groups',
     )
     user_permissions = models.ManyToManyField(
         Permission,
-        related_name='custom_user_permission_set',  # Уникальное имя для related_name
+        related_name='custom_user_permission_set',
+        # Уникальное имя для related_name
         blank=True,
         help_text='Specific permissions for this user.',
         verbose_name='user permissions',
@@ -93,23 +129,34 @@ class User(AbstractUser):
 
 
 class ProductCategory(models.Model):
+    """Модель связывающая товар и категории связью многие-ко-многим."""
+
     id = models.CharField(
         primary_key=True,
         max_length=18,
         null=False,
         verbose_name='Код'
     )
-    name = models.CharField(max_length=128, unique=True)
+    name = models.CharField(
+        max_length=128,
+        unique=True,
+        verbose_name='Название категории'
+    )
 
     class Meta:
+        """Русское название модели."""
+
         verbose_name = 'Категория',
         verbose_name_plural = 'Категории'
 
     def __str__(self):
+        """Возвращает название поля."""
         return self.name
 
 
 class Product(models.Model):
+    """Модель товара."""
+
     article = models.CharField(
         primary_key=True,
         max_length=16,
@@ -162,14 +209,19 @@ class Product(models.Model):
     )
 
     class Meta:
+        """Русское название модели."""
+
         verbose_name = 'Товар'
         verbose_name_plural = 'Товары'
 
     def __str__(self):
+        """Возвращает название поля."""
         return self.name
 
 
 class WarehouseProduct(models.Model):
+    """Связующая модель для таблиц Склад и Товар."""
+
     product = models.ForeignKey(
         Product,
         to_field='article',
@@ -185,6 +237,8 @@ class WarehouseProduct(models.Model):
     stock = models.IntegerField(null=True, default=0, verbose_name='Остаток')
 
     class Meta:
+        """Добавляет уникальное поле в качестве сочетания склада и товара."""
+
         constraints = [
             models.UniqueConstraint(fields=['warehouse', 'product'],
                                     name='unique_warehouse_product')
@@ -194,6 +248,8 @@ class WarehouseProduct(models.Model):
 
 
 class StoreProduct(models.Model):
+    """Связующая модель для таблиц Магазин и Товар."""
+
     store = models.ForeignKey(
         Store,
         on_delete=models.DO_NOTHING,
@@ -226,9 +282,12 @@ class StoreProduct(models.Model):
     )
 
     def __str__(self):
+        """Возвращает название товара и магазина."""
         return f'{self.product.name} - {self.store.name}'
 
     class Meta:
+        """Русское название модели."""
+
         constraints = [
             models.UniqueConstraint(fields=['store', 'product'],
                                     name='unique_store_product')
@@ -238,6 +297,8 @@ class StoreProduct(models.Model):
 
 
 class Comment(models.Model):
+    """Модель комментария."""
+
     text = models.TextField(verbose_name='Комментарий')
     created_at = models.DateTimeField(auto_now_add=True)
     finish_planned_date = models.DateTimeField(
@@ -268,15 +329,15 @@ class Comment(models.Model):
         max_length=56,
         null=False,
         default='Не выполнено',
-        verbose_name= 'Статус комментария'
+        verbose_name='Статус комментария'
     )
 
     class Meta:
+        """Русское название модели."""
+
         verbose_name = 'Комментарий'
         verbose_name_plural = 'Комментарии'
 
     def __str__(self):
+        """Возвращает название поля."""
         return self.text
-
-
-
