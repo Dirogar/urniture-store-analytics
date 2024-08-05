4c31dcce48619911b94e16be9055a87d9169f159
Max Ponomarenko <JustForLobster@gmail.com>
2024-08-05 15:48:45 +0500

    Добавлен маршрут для debug_toolbar

--- furniture_store/urls.py
+++ furniture_store/urls.py
@@ -14,12 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
+    path("__debug__/", include("debug_toolbar.urls")),
     path('admin/', admin.site.urls),
     path('', auth_views.LoginView.as_view(), name='login'),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
