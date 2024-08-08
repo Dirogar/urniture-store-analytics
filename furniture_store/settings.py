177315e7035ef0a7be6088a39ac319674e9b070a
Max Ponomarenko <JustForLobster@gmail.com>
2024-08-08 17:18:51 +0500

    Настройка дебаг мода вынесена в .env

--- furniture_store/settings.py
+++ furniture_store/settings.py
@@ -36,7 +36,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG_MODE')
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
