7f71137fabac5a7eebfb5a59385e5b4943327e75
Max Ponomarenko <JustForLobster@gmail.com>
2024-11-08 14:22:44 +0500

    Убраны разрешения для сваггера

--- furniture_store/urls.py
+++ furniture_store/urls.py
@@ -19,21 +19,18 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
-from rest_framework import permissions
+from rest_framework.permissions import AllowAny
 
 schema_view = get_schema_view(
     openapi.Info(
         title="API Documentation",
         default_version='v1',
         description="API documentation for the project",
-        terms_of_service="https://www.example.com/terms/",
-        contact=openapi.Contact(email="contact@example.com"),
-        license=openapi.License(name="BSD License"),
     ),
     public=True,
-    # permission_classes=(permissions.AllowAny,),
+    permission_classes=(AllowAny,),  # Обязательно AllowAny
+    authentication_classes=[]  # Отключаем аутентификацию полностью для этого представления
 )
-
 
 urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
